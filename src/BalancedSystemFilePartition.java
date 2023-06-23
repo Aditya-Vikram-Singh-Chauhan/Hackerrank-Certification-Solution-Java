727e9454c504c7c86493c5848630f80680fd29ad
AdityaSChauhan007 <61788499+Aditya-Vikram-Singh-Chauhan@users.noreply.github.com>
2023-06-23 11:37:05 +0530

    Update BalancedSystemFilePartition.java

--- src/BalancedSystemFilePartition.java
+++ src/BalancedSystemFilePartition.java
@@ -3,11 +3,51 @@ public class BalancedSystemFilePartition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Integer> parent = new ArrayList<>();
+		List<Integer> file_size = new ArrayList<>();
+		parent.add(-1); parent.add(0); parent.add(0); parent.add(1); parent.add(1); parent.add(2);
+		file_size.add(1); file_size.add(2); file_size.add(2); file_size.add(1); file_size.add(1); file_size.add(1);
 		
+		System.out.println("Most Balanced Partition : "+mostBalancedPartition(parent, file_size));
 		
-		//Solution
-		//https://github.com/anishLearnsToCode/hackerrank-problem-solving-skill-test/blob/master/src/BalancedSystemFilePartition.java
-
+		parent.clear();
+		file_size.clear();
+		parent.add(-1); parent.add(0); parent.add(1); parent.add(2);
+		file_size.add(1); file_size.add(4); file_size.add(3); file_size.add(4);
+		
+		System.out.println("Most Balanced Partition : "+mostBalancedPartition(parent, file_size));
+		
+		parent.clear();
+		file_size.clear();
+		parent.add(-1); parent.add(0); parent.add(0); parent.add(0);
+		file_size.add(10); file_size.add(11); file_size.add(10); file_size.add(10);
+		
+		System.out.println("Most Balanced Partition : "+mostBalancedPartition(parent, file_size));
 	}
+		
+		public static int mostBalancedPartition(List<Integer> parent, List<Integer> files_size) {
+	        int n = parent.size();
+	        ArrayList<Integer>[] children = new ArrayList[n];
+	        for (int i = 0; i < n; i++) {
+	            children[i] = new ArrayList<>();
+	        }
+	        for (int i = 1; i < n; i++) {
+	            children[parent.get(i)].add(i);
+	        }
+	        Integer[] size_sums = new Integer[n];
+	        size_sums_rec(0, files_size, children, size_sums);
+	        int min = Integer.MAX_VALUE;
+	        for (int i = 1; i < n; i++) {
+	            min = Math.min(min, Math.abs(size_sums[0] - 2 * size_sums[i]));
+	        }
+	        return min;
+	}
+		private static int size_sums_rec(int i, List<Integer> files_size, ArrayList<Integer>[] children, Integer[] size_sums) {
+	        size_sums[i] = files_size.get(i);
+	        for (int c : children[i]) {
+	            size_sums[i] += size_sums_rec(c, files_size, children, size_sums);
+	        }
+	        return size_sums[i];
+	    }
 
 }
