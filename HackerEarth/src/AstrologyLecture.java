eb861615a6fcba91c96349e5fe88764fcf91fdc5
AdityaSChauhan007 <61788499+Aditya-Vikram-Singh-Chauhan@users.noreply.github.com>
2023-07-03 14:46:56 +0530

    Update AstrologyLecture.java

--- HackerEarth/src/AstrologyLecture.java
+++ HackerEarth/src/AstrologyLecture.java
@@ -10,37 +10,59 @@ public class AstrologyLecture {
 	}
 
 	private static void getTrapeziumPattern(int n) {
-		int i, j;
-		for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                if (j < n - i - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(".");
-            }
-            for (j = 0; j < n - 1; j++) {
-                if (j < i)
-                    System.out.print(".");
-                else
-                    System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (i = 2; i <= n; i++) {
-            for (j = 0; j < n; j++) {
-                if (j < i - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(".");
-            }
-            for (j = 0; j < n - 1; j++) {
-                if (j < n - i)
-                    System.out.print(".");
-                else
-                    System.out.print("*");
-            }
-            System.out.println();
-        }
+		String[] arr = new String[2*n-1];
+		String star = "*";
+		String dot = ".";
+		for(int i=0; i< arr.length; i++) {
+			arr[i] = star.repeat(2*n - 1);
+		}
+		//System.out.println(Arrays.deepToString(arr));
+		List<String> res = new ArrayList<>();
+		for(int i=1; i<=n ; i++) {
+			arr[i-1] = arr[i-1].substring(0,n-i)+ dot.repeat(i);
+			arr[i-1] = arr[i-1] + new StringBuffer(arr[i-1]).reverse().substring(1);
+			//System.out.println(arr[i-1]);
+			res.add(arr[i-1]);
+		}
+		List<String> result = new ArrayList<>();
+		result.addAll(res);
+		Collections.reverse(res);
+		res.remove(0);
+		result.addAll(res);
+		for(String s : result) {
+			System.out.println(s);
+		}
+		// int i, j;
+		// for (i = 0; i < n; i++) {
+  //           for (j = 0; j < n; j++) {
+  //               if (j < n - i - 1)
+  //                   System.out.print("*");
+  //               else
+  //                   System.out.print(".");
+  //           }
+  //           for (j = 0; j < n - 1; j++) {
+  //               if (j < i)
+  //                   System.out.print(".");
+  //               else
+  //                   System.out.print("*");
+  //           }
+  //           System.out.println();
+  //       }
+  //       for (i = 2; i <= n; i++) {
+  //           for (j = 0; j < n; j++) {
+  //               if (j < i - 1)
+  //                   System.out.print("*");
+  //               else
+  //                   System.out.print(".");
+  //           }
+  //           for (j = 0; j < n - 1; j++) {
+  //               if (j < n - i)
+  //                   System.out.print(".");
+  //               else
+  //                   System.out.print("*");
+  //           }
+  //           System.out.println();
+  //       }
 	}
 
 }
