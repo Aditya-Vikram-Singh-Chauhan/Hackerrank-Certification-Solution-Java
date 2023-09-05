cfa3cc2360ae347f4fd7888d176b2375358905de
AdityaSChauhan007 <61788499+Aditya-Vikram-Singh-Chauhan@users.noreply.github.com>
2023-09-05 14:03:51 +0530

    Delete src4/src directory

--- src4/src/main/java/com/ideaproject/domaininfo/Testing.java
+++
@@ -1,9 +0,0 @@
-package com.ideaproject.domaininfo;
-
-public class Testing {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world santhana");
-    }
-    }
-
--- src4/src/main/java/com/ideaproject/domaininfo/configuration/SecretsManagerServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ideaproject.domaininfo.configuration;
-
-import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-
-public class SecretsManagerServiceImpl extends SecretsManagerService {
-    public SecretsManagerServiceImpl(SecretsManagerClient build) {
-        super();
-    }
-}
